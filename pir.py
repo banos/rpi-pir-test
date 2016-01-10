e5656f91c46aeb25ec9fdee7379445e972c7cbda
banos <git@solomou.com>
2016-01-10 21:24:11 +0000

    add mp3 sound on detection; handle ctrl+c; increase init wait

--- pir.py
+++ pir.py
@@ -3,6 +3,8 @@
 import RPi.GPIO as GPIO
 import time
 import logging
+#from espeak import espeak
+import subprocess
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -10,29 +12,42 @@ logging.basicConfig(level=logging.INFO,
 
 
 logging.info ("Test ctrl+c to exit")
-logging.info ("Initialising...")
+logging.info ("Initialising... 10sec...")
 
 GPIO.setmode(GPIO.BCM)
 
 PIR_PIN=4
 
-GPIO.setup(PIR_PIN, GPIO.IN, GPIO.PUD_DOWN)
+try:
+    GPIO.setup(PIR_PIN, GPIO.IN, GPIO.PUD_DOWN)
 
-time.sleep(2)
-logging.info ("Ready")
+    time.sleep(10)
+    logging.info ("Ready for action...")
 
-prev_state = False
-curr_state = False
+    prev_state = False
+    curr_state = False
 
-while True:
-   prev_state = curr_state
-   curr_state = GPIO.input(PIR_PIN)
+    while True:
+       prev_state = curr_state
+       curr_state = GPIO.input(PIR_PIN)
 
-   logging.debug ("%s" % curr_state)
+       logging.debug ("%s" % curr_state)
 
-   if curr_state != prev_state:
-      new_state = "HIGH" if curr_state else "LOW"
-      logging.info ("Motion detected!!! %s" % (new_state))
-   time.sleep(0.1)
+       if curr_state != prev_state:
+          new_state = "HIGH" if curr_state else "LOW"
+          logging.debug ("Motion detected!!! %s" % (new_state))
+
+          if new_state == "HIGH":
+               logging.info ("Motion detected! (STATE=HIGH)")
+               #espeak.synth("Step into my office")
+               subprocess.call ( ["omxplayer", "-o", "hdmi", "evil_laugh.mp3"] )
+          else:
+               logging.info ("Reset (STATE=LOW)")
+
+       time.sleep(0.1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    logging.info ("Goodbye.")
+
 
 
